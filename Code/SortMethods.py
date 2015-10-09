f7a9a5f43906f3108ddaddc61a7958ade8bda7bc
Jiankai <jiankai@Jiankais-MacBook-Pro.local>
2015-10-09 14:47:45 -0400

    add insert_sort

--- Code/SortMethods.py
+++ Code/SortMethods.py
@@ -1,7 +1,24 @@
-"""
-quick sort with Python
-"""
+
+def insert_sort(lists):
+    for i in range(1,len(lists)):
+        key = lists[i]
+        j = i - 1
+        while j >= 0:
+            if lists[j] > key:
+                lists[j], lists[j+1] = key, lists[j]
+            j -= 1
+    # lists is a mutable variable, has been changed
+    return lists
+
+
+
+
+
+
 def quicksort(array):
+    """
+    quick sort with Python
+    """
     less = []
     greater = []
     if len(array) == 1 or len(array) == 0:
@@ -15,5 +32,7 @@ def quicksort(array):
     return quicksort(less) + [pivort] + quicksort(greater)
 
 
-array = [-1,2,4,12,2,434,45,12,34,2323]
-print quicksort(array)
+array = [-1,2,98,23,4,12,2,434,45,12,34,2323]
+#print quicksort(array)
+insert_sort(array)
+print array
